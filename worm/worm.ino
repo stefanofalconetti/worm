ae06b7514996ae576bd533e61db453fb37599b5c
C012426 <C012426@5M2VF72.galileo.corp.lcl>
2017-01-15 00:31:04 +0100

    Serial port handling

--- worm/worm.ino
+++ worm/worm.ino
@@ -1,51 +1,71 @@
 #include "Arduino.h"
 #include <Servo.h>
 
+const int SERIAL_0_SPEED = 9600;
+const int SERIAL_1_SPEED = 57600;
+const int SERIAL_1_READ_UNTIL_TIMEOUT_MILLIS = 100;
+
+const char COMMAND_DELIMITER = '#';
+
 Servo SERVO_1;
-int SERVO_1_PIN = 2;
-int SERVO_1_MIN_ANGLE = 45;
-int SERVO_1_MAX_ANGLE = 175;
-int SERVO_1_MID_ANGLE = 95;
+const int SERVO_1_PIN = 2;
+const int SERVO_1_MIN_ANGLE = 45;
+const int SERVO_1_MAX_ANGLE = 175;
+const int SERVO_1_MID_ANGLE = 95;
 
 Servo SERVO_2;
-int SERVO_2_PIN = 3;
-int SERVO_2_MIN_ANGLE = 45;
-int SERVO_2_MAX_ANGLE = 175;
-int SERVO_2_MID_ANGLE = 95;
+const int SERVO_2_PIN = 3;
+const int SERVO_2_MIN_ANGLE = 45;
+const int SERVO_2_MAX_ANGLE = 175;
+const int SERVO_2_MID_ANGLE = 95;
 
 Servo SERVO_3;
-int SERVO_3_PIN = 4;
-int SERVO_3_MIN_ANGLE = 45;
-int SERVO_3_MAX_ANGLE = 175;
-int SERVO_3_MID_ANGLE = 95;
+const int SERVO_3_PIN = 4;
+const int SERVO_3_MIN_ANGLE = 45;
+const int SERVO_3_MAX_ANGLE = 175;
+const int SERVO_3_MID_ANGLE = 95;
 
 Servo SERVO_4;
-int SERVO_4_PIN = 5;
-int SERVO_4_MIN_ANGLE = 45;
-int SERVO_4_MAX_ANGLE = 175;
-int SERVO_4_MID_ANGLE = 95;
+const int SERVO_4_PIN = 5;
+const int SERVO_4_MIN_ANGLE = 45;
+const int SERVO_4_MAX_ANGLE = 175;
+const int SERVO_4_MID_ANGLE = 95;
 
 Servo SERVO_5;
-int SERVO_5_PIN = 8;
-int SERVO_5_MIN_ANGLE = 45;
-int SERVO_5_MAX_ANGLE = 175;
-int SERVO_5_MID_ANGLE = 95;
+const int SERVO_5_PIN = 8;
+const int SERVO_5_MIN_ANGLE = 45;
+const int SERVO_5_MAX_ANGLE = 175;
+const int SERVO_5_MID_ANGLE = 95;
 
 Servo SERVO_6;
-int SERVO_6_PIN = 9;
-int SERVO_6_MIN_ANGLE = 45;
-int SERVO_6_MAX_ANGLE = 175;
-int SERVO_6_MID_ANGLE = 95;
+const int SERVO_6_PIN = 9;
+const int SERVO_6_MIN_ANGLE = 45;
+const int SERVO_6_MAX_ANGLE = 175;
+const int SERVO_6_MID_ANGLE = 95;
 
 Servo SERVO_7;
-int SERVO_7_PIN = 10;
-int SERVO_7_MIN_ANGLE = 45;
-int SERVO_7_MAX_ANGLE = 175;
-int SERVO_7_MID_ANGLE = 95;
+const int SERVO_7_PIN = 10;
+const int SERVO_7_MIN_ANGLE = 45;
+const int SERVO_7_MAX_ANGLE = 175;
+const int SERVO_7_MID_ANGLE = 95;
 
 //The setup function is called once at startup of the sketch
 void setup() {
 // Add your initialization code here
+
+	initServos();
+	setWormPositionTo_FullExtension();
+	initRadioSerialPort();
+	Serial.begin(SERIAL_0_SPEED);
+}
+
+// The loop function is called in an endless loop
+void loop() {
+//Add your repeated code here
+	readSerialMessage();
+}
+
+void initServos() {
 	SERVO_1.attach(SERVO_1_PIN, SERVO_1_MIN_ANGLE, SERVO_1_MAX_ANGLE);
 	delay(1000);
 	SERVO_2.attach(SERVO_2_PIN, SERVO_2_MIN_ANGLE, SERVO_2_MAX_ANGLE);
@@ -59,20 +79,10 @@ void setup() {
 	SERVO_6.attach(SERVO_6_PIN, SERVO_6_MIN_ANGLE, SERVO_6_MAX_ANGLE);
 	delay(1000);
 	SERVO_7.attach(SERVO_7_PIN, SERVO_7_MIN_ANGLE, SERVO_7_MAX_ANGLE);
-
-	fullExtension();
 }
 
-// The loop function is called in an endless loop
-void loop() {
-//Add your repeated code here
-}
-
-void fullExtension() {
+void setWormPositionTo_FullExtension() {
 	SERVO_1.write(SERVO_1_MID_ANGLE);
-	SERVO_2.write(SERVO_2_MIN_ANGLE);
-	SERVO_3.write(SERVO_3_MAX_ANGLE);
-	/*
 	delay(1000);
 	SERVO_2.write(SERVO_2_MID_ANGLE);
 	delay(1000);
@@ -85,5 +95,23 @@ void fullExtension() {
 	SERVO_6.write(SERVO_6_MID_ANGLE);
 	delay(1000);
 	SERVO_7.write(SERVO_7_MID_ANGLE);
-	*/
+}
+
+void initRadioSerialPort(){
+	Serial1.begin(SERIAL_1_SPEED);
+	Serial1.setTimeout(SERIAL_1_READ_UNTIL_TIMEOUT_MILLIS);
+}
+
+/**
+ * Read a message from the serial line that contains a command to be executed
+ */
+String readSerialMessage() {
+
+	String commandString;
+
+	if (Serial1.available()) {
+		commandString = Serial1.readStringUntil(COMMAND_DELIMITER);
+		Serial.print("Received: [" + commandString + "]");
+	}
+	return commandString;
 }
